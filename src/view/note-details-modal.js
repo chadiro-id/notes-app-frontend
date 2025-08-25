0956fd4abe15b7c26ab3465faa42790679b9d36e
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-25 14:59:46 +0700

    New: scripts for managing note details modal

---
+++ src/view/note-details-modal.js
@@ -0,0 +1,76 @@
+const modal = (closeCallback) => {
+  const dialog = document.getElementById('note-details-modal');
+  const modalBox = dialog.querySelector('.modal-box');
+  const closeBtn = dialog.querySelector('.modal__close-button');
+
+  const noteTitleEl = dialog.querySelector('.note-details__title');
+  const noteBodyEl = dialog.querySelector('.note-details__body');
+  const noteCreatedAtEl = dialog.querySelector('.note-details__created-at');
+  const noteArchiveToggle = dialog.querySelector('.note-details__archive-toggle');
+  const noteDeleteBtn = dialog.querySelector('.note-details__delete-button');
+
+  let _data = {};
+
+  closeBtn.addEventListener('click', () => {
+    dialog.close('close');
+  });
+
+  closeBtn.addEventListener('keydown', (evt) => {
+    evt.shiftKey
+      && evt.key === 'Tab'
+      && (evt.preventDefault(), noteDeleteBtn.focus());
+  });
+
+  noteArchiveToggle.addEventListener('click', (evt) => {
+    evt.stopPropagation();
+    dialog.close('archive');
+  });
+
+  noteDeleteBtn.addEventListener('click', (evt) => {
+    evt.stopPropagation();
+    dialog.close('delete');
+  });
+
+  noteDeleteBtn.addEventListener('keydown', (evt) => {
+    !evt.shiftKey
+      && evt.key === 'Tab'
+      && (evt.preventDefault(), closeBtn.focus());
+  });
+
+  dialog.addEventListener('click', (evt) => {
+    evt.stopPropagation();
+    if (!modalBox.contains(evt.target)) {
+      dialog.close();
+    }
+  });
+
+  dialog.addEventListener('close', () => {
+    if (closeCallback) {
+      closeCallback(dialog.returnValue, _data);
+    }
+  });
+
+  const show = (data) => {
+    _data = data;
+    noteTitleEl.textContent = data.title;
+    noteBodyEl.innerText = data.body;
+    noteCreatedAtEl.textContent = new Date(data.createdAt).toLocaleString();
+    noteArchiveToggle.textContent = data.archived ? 'Unarchive' : 'Archive';
+
+    dialog.showModal();
+  };
+
+  return {
+    // show({ title, body, createdAt, archived }) {
+    //   noteTitleEl.textContent = title;
+    //   noteBodyEl.innerText = body;
+    //   noteCreatedAtEl.textContent = new Date(createdAt).toLocaleString();
+    //   noteArchiveToggle.textContent = archived ? 'Unarchive' : 'Archive';
+
+    //   dialog.showModal();
+    // }
+    show
+  };
+};
+
+export default modal;
