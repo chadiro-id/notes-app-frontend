aa2f964822a5759e238319d1d70c1012fcd459c6
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-09-05 14:08:11 +0700

    Implement notes empty and loading ui

---
+++ src/view/notes-empty.js
@@ -0,0 +1,21 @@
+const NotesEmpty = () => {
+  const el = document.getElementById("notes-empty");
+  const messageEl = el.querySelector(".notes-empty__message");
+
+  const show = (message) => {
+    messageEl.textContent = message;
+    el.classList.toggle("notes-empty--hidden", false);
+  }
+
+  const hide = () => {
+    el.classList.toggle("notes-empty--hidden", true);
+  }
+
+  return {
+    show,
+    hide
+  };
+}
+
+
+export default NotesEmpty;
---
+++ src/view/notes-skeleton.js
@@ -0,0 +1,32 @@
+const NotesSkeleton = () => {
+  const el = document.getElementById("notes-skeleton");
+  const skeletons = el.querySelectorAll(".skeleton");
+
+  const play = () => {
+    if (el.hidden) {
+      el.hidden = false
+    }
+
+    skeletons.forEach((item) => {
+      item.classList.add("skeleton--animate");
+    });
+  }
+
+  const stop = () => {
+    skeletons.forEach((item) => {
+      item.classList.remove("skeleton--animate");
+    });
+  }
+
+  const hide = () => {
+    el.hidden = true;
+  }
+
+  return {
+    play,
+    stop,
+    hide
+  }
+}
+
+export default NotesSkeleton;
